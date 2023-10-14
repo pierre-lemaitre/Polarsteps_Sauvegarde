39eca8d08b086826ef9573d4244c9f8a4b8a74a5
pierre-lemaitre <118831263+pierre-lemaitre@users.noreply.github.com>
2023-10-14 15:38:02 +0200

    Fichier Powershell renommage.ps1

---
+++ renommage.ps1
@@ -0,0 +1,48 @@
+Write-host "------------------------------------------------------"
+Write-host "Le renommage des fichiers est en cours. Patientez ...."
+Write-host "------------------------------------------------------"
+$q = dir -Recurse | Where-Object {$_.name -like "*jpg*"}
+$temp_dir=''
+$a = 0
+foreach ($qs in $q)
+{
+
+    $name_dir = Split-Path -Path (Get-Location) -Leaf
+
+    $chemin_complet = Get-ChildItem -Path $qs -Recurse | Select-Object -ExpandProperty FullName 
+
+    $chemin_dir = Split-Path -Path $chemin_complet -Parent
+
+    if ($chemin_dir -ne $temp_dir) {
+        $a=0
+    }
+    $a = ($a + 1)
+    $name = $qs.FullName
+    Rename-Item $name -NewName "$a.jpg"
+    $temp_dir = $chemin_dir
+}
+
+$q = dir -Recurse | Where-Object {$_.name -like "*mp4*"}
+$temp_dir=''
+$a = 0
+foreach ($qs in $q)
+{
+
+    $name_dir = Split-Path -Path (Get-Location) -Leaf
+
+    $chemin_complet = Get-ChildItem -Path $qs -Recurse | Select-Object -ExpandProperty FullName 
+
+    $chemin_dir = Split-Path -Path $chemin_complet -Parent
+
+    if ($chemin_dir -ne $temp_dir) {
+        $a=0
+    }
+    $a = ($a + 1)
+    $name = $qs.FullName
+    Rename-Item $name -NewName "$a.mp4"
+    $temp_dir = $chemin_dir
+}
+Write-host "----------------------------------"
+Write-host "Le renommage des fichiers est fini"
+Write-host "----------------------------------"
+pause
